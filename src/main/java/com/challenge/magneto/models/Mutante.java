8b8c7cd037fadf8ef449714cfe5d6963e02c1c66
leandro <leandroandresvillegas@gmail.com>
2022-03-11 17:39:07 -0300

    Se agrega el proyecto

---
+++ src/main/java/com/challenge/magneto/models/Mutante.java
@@ -0,0 +1,23 @@
+package com.challenge.magneto.models;
+
+import org.json.JSONObject;
+
+public class Mutante {
+
+	private String [] dna;
+
+	public String [] getDna() {
+		return dna;
+	}
+
+	public void setDna(String [] dna) {
+		this.dna = dna;
+	}
+
+	public JSONObject getJSONObject(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
